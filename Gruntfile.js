aeb9c497db9a1ca621836613be777ad129bbebed
Paul Rodwell <paul.rodwell@btinternet.com>
2026-09-10 17:19:58 +0100

    update devDependencies and trim

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,33 +1,22 @@
 module.exports = function (grunt) {
-  grunt.loadNpmTasks('grunt-browserify');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-git-authors');
+  grunt.loadNpmTasks("grunt-browserify");
+  grunt.loadNpmTasks("grunt-git-authors");
 
   grunt.initConfig({
-
     browserify: {
       plugin: {
-        src: ['client/security.coffee'],
-        dest: 'client/security.js',
+        src: ["client/security.coffee"],
+        dest: "client/security.js",
         options: {
-          transform: ['coffeeify'],
+          transform: ["coffeeify"],
           browserifyOptions: {
-            extentions: ".coffee"
-          }
-        }
-      }
+            extentions: ".coffee",
+          },
+        },
+      },
     },
-
-    watch: {
-      all: {
-        files: ['client/*.coffee'],
-        tasks: ['build']
-      }
-
-    }
   });
 
-  grunt.registerTask('build', ['browserify']);
-  grunt.registerTask('default', ['build']);
-
+  grunt.registerTask("build", ["browserify"]);
+  grunt.registerTask("default", ["build"]);
 };
